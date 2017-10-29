7ef83248f4f4efb188341e7899a9b7ffd7bcc2c6
Rodrigo Akad <rodrigoakad@hotmail.com>
2017-10-29 16:50:42 -0200

    Initial Setup

---
+++ GameShare/GameShare/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GameShare.Startup))]
+namespace GameShare
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
